1751bfbb4b9d9d6b2886edd57c7c3f71df65c4f4
lianhong <2758988938@qq.com>
2023-11-27 11:36:13 +0800

    1

--- main.go
+++ main.go
@@ -5,9 +5,8 @@ import (
 	"github.com/lianhong2758/RosmBot-MUL/rosm"
 
 	//"github.com/lianhong2758/RosmBot-MUL/server/mys"
-	//_ "github.com/lianhong2758/RosmBot-MUL/server/mys/init"
+	_ "github.com/lianhong2758/RosmBot-MUL/server/mys/init"
 	//_ "github.com/lianhong2758/RosmBot-MUL/server/qq/init"
-	"github.com/lianhong2758/RosmBot-MUL/server/qq"
 
 	//_ "github.com/lianhong2758/RosmBot-MUL/plugins/myplugin"
 	_ "github.com/lianhong2758/RosmBot-MUL/plugins/chatgpt"
@@ -16,9 +15,9 @@ import (
 )
 
 func main() {
-
-	var runner rosm.Boter = qq.NewConfig("config/qq2.json")
-	go runner.Run()
-
+	/*
+		var runner rosm.Boter = mys.NewConfig("config/qq2.json")
+		go runner.Run()
+	*/
 	rosm.Listen()
 }
