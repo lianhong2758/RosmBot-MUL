2c5e20d30c5f3e79dd583dd127c41a7e9dd0db3a
lianhong <2758988938@qq.com>
2024-04-04 14:20:21 +0800

    更新main的结构

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 	//Bot注册
-	//"github.com/lianhong2758/RosmBot-MUL/server/mys"
 	//_"github.com/lianhong2758/RosmBot-MUL/server/mys/init"
 	//_ "github.com/lianhong2758/RosmBot-MUL/server/qq/init"
+	//"github.com/lianhong2758/RosmBot-MUL/server/ob11"
 	_ "github.com/lianhong2758/RosmBot-MUL/server/ob11/init"
 	//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
@@ -24,7 +24,7 @@ import (
 
 func main() {
 	//下面两行用于同平台多bot构建,不需要可以忽略
-	// var runner rosm.Boter = mys.NewConfig("config/mys.json")
+	// var runner rosm.Boter = ob11.NewConfig("config/ob11.json")
 	// go runner.Run()
 
 	//阻塞主进程
