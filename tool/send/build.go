1a4d68440a8f74a72e46e557129bddd1c8030bd3
lianhong <2758988938@qq.com>
2024-05-07 22:23:43 +0800

    格式化

--- tool/send/build.go
+++ tool/send/build.go
@@ -23,12 +23,12 @@ func CTXBuild(types, botid, roomid12 string) (ctx *rosm.Ctx) {
 			botid = qq.GetRandBot().BotID
 		}
 		ctx = qq.NewCTX(botid, types, id1, id2)
-	case "ob11":		
-	group, _ := tool.SplitPadString(roomid12)
-	if botid == "" {
-		botid = ob11.GetRandBot().BotID
-	}
-	ctx =ob11.NewCTX(botid,   group)
+	case "ob11":
+		group, _ := tool.SplitPadString(roomid12)
+		if botid == "" {
+			botid = ob11.GetRandBot().BotID
+		}
+		ctx = ob11.NewCTX(botid, group)
 	}
 	return ctx
 }
