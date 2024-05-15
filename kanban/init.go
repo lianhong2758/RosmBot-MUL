886fd1cf7ee1578d673784b1b5f483dbf9ff432b
lianhong <2758988938@qq.com>
2024-05-15 18:07:08 +0800

    fix

--- kanban/init.go
+++ kanban/init.go
@@ -11,12 +11,12 @@ func init() {
 	//log_file
 	path := flag.String("lf", "data/log/"+time.Now().Format("2006_01_02_15_04_05")+".log", "log file path")
 	notSaveLogs := flag.Bool("nolf", false, "不保存日志文件")
-	if !*notSaveLogs {
-		SetLogWithNewFile(*path)
-	}
 	//debug
 	d := flag.Bool("d", false, "Enable debug level log and higher.")
 	flag.Parse()
+	if !*notSaveLogs {
+		SetLogWithNewFile(*path)
+	}
 	if *d {
 		log.SetLevel(log.DebugLevel)
 	}
